da7473cd39f604279784d77134104f759c8fe982
RashmitShrestha <rashmit_stha@gmail.com>
2020-07-30 15:19:03 -0700

    Updated

--- clockapp.js
+++ clockapp.js
@@ -1,6 +1,5 @@
 const honelm = document.getElementById("honelm");
 
-
 var work = new Audio('work.mp3');
 
 var breakTime = new Audio('break.m4a');
@@ -18,8 +17,6 @@ function beGreen() {
     breakTime.play();
 
 }
-
-
 
 document.getElementById("yeet").addEventListener("click",   () => {
     function megFunc() {
@@ -42,12 +39,10 @@ var brksec = document.getElementById("brksec").value;
 var wrkmin = document.getElementById("wrkmin").value;
 var wrksec = document.getElementById("wrksec").value;
 
-
 if (Number(brkmin) == null) {
 brkmin = 0;
 
 }
-
 
 if (Number(brksec) == null) {
     brksec = 0;
@@ -55,21 +50,14 @@ if (Number(brksec) == null) {
     }
     if (Number(wrkmin) == null) {
         wrkmin = 0;
-    
     }
     if (Number(wrksec) == null) {
         wrksec = 0;
-    
+
     }
-
-
-
-
-
 
 var totBrkTime = Number((brkmin * 60000) + (brksec * 1000));
 var totWrkTime = Number((wrkmin * 60000) + (wrksec * 1000));
-
 
     setTimeout(() => {
 
@@ -82,9 +70,6 @@ var totWrkTime = Number((wrkmin * 60000) + (wrksec * 1000));
     },totWrkTime + totBrkTime)
 
 },totWrkTime)
-
-
-
 
 
 }        )
