43b1e31533cce55a3c929a8c86146e587ccdf59f
RashmitShrestha <rashmit_stha@gmail.com>
2021-03-10 23:54:06 -0800

    Updated

--- clockapp.js
+++ clockapp.js
@@ -1,4 +1,3 @@
-
 const honelm = document.getElementById("honelm");
 const timeLeft = document.getElementById("timeLeft");
 
@@ -12,57 +11,48 @@ document.getElementById("yeet").addEventListener("click", () => {
   var wrkmin = document.getElementById("wrkmin").value;
   var wrksec = document.getElementById("wrksec").value;
 
-  if (Number(brkmin) == null) {
-    brkmin = 0;
-  }
+  // if (Number(brkmin) == null) {
+  //   brkmin = 0;
+  // }
 
-  if (Number(brksec) == null) {
-    brksec = 0;
-  }
-  if (Number(wrkmin) == null) {
-    wrkmin = 0;
-  }
-  if (Number(wrksec) == null) {
-    wrksec = 0;
-  }
+  // if (Number(brksec) == null) {
+  //   brksec = 0;
+  // }
+  // if (Number(wrkmin) == null) {
+  //   wrkmin = 0;
+  // }
+  // if (Number(wrksec) == null) {
+  //   wrksec = 0;
+  // }
 
-
-
-
-
-(function myFunc01() {
-
-  //YOUR TIME IN HELL HAS BEGUN
-  honelm.style.color = "red";
-  honelm.innerHTML = "GET WORKIN'";
-
-  let iWrk =Number( wrkmin * 60 + wrksec);
-  let iBrk = Number(brkmin * 60 + brksec);
+  (function myFunc01() {
+    //YOUR TIME IN HELL HAS BEGUN
+    honelm.style.color = "red";
+    honelm.innerHTML = "GET WORKIN'";
+work.play();
+    let iWrk = Number(wrkmin * 60 + wrksec);
+    let iBrk = Number(brkmin * 60 + brksec );
 
     // store the interval id to clear in future
     var intr = setInterval(function () {
-      console.log(typeof(iWrk)); // clear the interval if `i` reached 100
+
+      timeLeft.innerHTML = new Date(Math.abs(iWrk -1) * 1000).toISOString().substr(11, 8);
+
       if (--iWrk < 0) {
         clearInterval(intr);
         honelm.style.color = "green";
         honelm.innerHTML = "Break Time!";
+        breakTime.play();
+
         // store the interval id to clear in future
         var intW = setInterval(function () {
-          console.log(iBrk); // clear the interval if `i` reached 100
+          timeLeft.innerHTML = new Date(Math.abs(iBrk -1 ) * 1000).toISOString().substr(11, 8);
           if (--iBrk < 0) {
             clearInterval(intW);
             myFunc01();
           }
         }, 1000);
-    
-
-
-
       }
     }, 1000);
-
-}
-)
-
+  })();
 });
-
